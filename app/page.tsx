39e9de70066d070ab74023394ec87f5a8c71de3d
orrvan2 <973658027@qq.com>
2023-02-07 16:26:54 +0800

    aixuexi Beta

---
+++ app/page.tsx
@@ -0,0 +1,145 @@
+'use client'
+import React, { useState } from 'react'
+export default function Home() {
+  const [request, setRequest] = useState<{content?: string, subject?: string,parsing?: string}>({})
+  let [itinerary, setItinerary] = useState<string>('')
+
+  const [loading, setLoading] = useState(false)
+  const [message, setMessage] = useState('')
+  async function hitAPI() {
+    if (!request.content) return
+    setMessage('AI老师正在读题...')
+    setLoading(true)
+    setItinerary('')
+
+    setTimeout(() => {
+      setMessage('AI老师正在解题 ...')
+    }, 7000)
+
+    setTimeout(() => {
+      setMessage('AI老师正在组织语言 ...')
+    }, 15000)
+    try{
+      const response = await fetch('/api/get-itinerary', {
+        method: 'POST',
+        body: JSON.stringify({
+          content: request.content,
+          subject: request.subject,
+          parsing : request.parsing
+        })
+      })
+      const json = await response.json()
+      
+      // const response2 = await fetch('/api/get-points-of-interest', {
+      //   method: 'POST',
+      //   body: JSON.stringify({
+      //     pointsOfInterestPrompt: json.pointsOfInterestPrompt,
+      //   })
+      // })
+      // const json2 = await response2.json()
+  
+      // let pointsOfInterest = JSON.parse(json2.pointsOfInterest)
+      let itinerary = json.itinerary
+  
+      // console.log('pointsOfInterest: ', pointsOfInterest)
+      // pointsOfInterest.map(point => {
+      //   // itinerary = itinerary.replace(point, `<a target="_blank" rel="no-opener" href="https://www.google.com/search?q=${encodeURIComponent(point + ' ' + request.subject)}">${point}</a>`)
+      //   itinerary = itinerary.replace(point, `[${point}](https://www.baidu.com/s?wd=${encodeURIComponent(point + ' ' + request.subject)})`)
+      // })
+  
+      setItinerary(itinerary)
+      setLoading(false)
+    }catch (err) {
+      alert('AI老师正在忙，请稍后再试一次')
+      console.log('error: ', err)
+    }
+  }
+  // let content = itinerary.split('第')
+  console.log(itinerary)
+  // if (content.length > 1) {
+  //   content.shift()
+  // } else {
+  //   content[0] = "1" + content[0]
+  // }
+  console.log(request)
+  return (
+    <main>
+      <div className="app-container">
+        <h1 style={styles.header}>'爱提问' 你的人工智能老师</h1>
+        <div style={styles.formContainer} className="form-container">
+          {/* <input style={styles.input}  placeholder="想去旅游的城市" onChange={e => setRequest(request => ({
+            ...request, subject: e.target.value
+          }))} /> */}
+          <select style={styles.input} name="subject" onChange={e => setRequest(request => ({
+            ...request, subject: e.target.value
+          }))}>
+            <option value="生物">生物</option>
+            <option value="物理">物理</option>
+            <option value="数学">数学</option>
+            <option value="化学">化学</option>
+            <option value="英语">英语</option>
+            <option value="计算机">计算机</option>
+            <option value="地理">地理</option>
+          </select >
+          <input style={styles.input} placeholder="问题内容，建议复制粘贴" onChange={e => setRequest(request => ({
+            ...request, content: e.target.value
+          }))} />
+          <select style={styles.input} name="parsing" onChange={e => setRequest(request => ({
+            ...request, parsing: e.target.value
+          }))}>
+            <option value="不需要">不需要详细解析</option>
+            <option value="需要">需要详细解析</option>
+          </select >
+          {/* <input style={styles.input} placeholder="是否需要详细解析" onChange={e => setRequest(request => ({
+            ...request, parsing: e.target.value
+          }))} />           */}
+          <button className="input-button"  onClick={hitAPI}>开始你的提问</button>
+        </div>
+        <div className="results-container">
+        {
+          loading && (
+            <p>{message}</p>
+          )
+        }
+        {
+          itinerary && <div style={styles.content} >{itinerary}</div>
+        }
+
+        </div>
+      </div>
+    </main>
+  )
+}
+
+const styles = {
+  header: {
+    textAlign: 'center' as 'center',
+    marginTop: '80px',
+    color: '#c683ff',
+    fontSize: '44px'
+  },
+  input: {
+    padding: '10px 14px',
+    marginBottom: '4px',
+    outline: 'none',
+    fontSize: '16px',
+    width: '100%',
+    borderRadius: '8px'
+  },
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column' as 'column',
+    margin: '30px auto 0px',
+    padding: '20px',
+    boxShadow: '0px 0px 12px rgba(198, 131, 255, .2)',
+    borderRadius: '10px'
+  },
+  result: {
+    color: 'white'
+  },
+  content:{
+    marigin:'0 auto',
+    color:'white'
+  }
+
+}
---
+++ pages/api/get-itinerary.ts
@@ -0,0 +1,52 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  message: string,
+  itinerary: any,
+}
+
+const GPT_KEY = process.env.GPT_API_KEY
+
+const headers = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${GPT_KEY}`
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  let content = '你好', subject = '生物', parsing='不需要'
+  if (req.body) {
+    let body = JSON.parse(req.body)
+    content = body.content ||content
+    subject = body.subject ||subject
+    parsing = body.parsing ||parsing
+  }
+
+  let basePrompt = `我想咨询关于${subject}学科的问题，问题内容是：${content},${parsing}详细解析。如果问题内容和${subject}学科无关，请拒绝回答。 `
+  try {
+    const response = await fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt: basePrompt,
+        temperature: 0,
+        max_tokens: 600
+      })
+    })
+    try{
+      const itinerary = await response.json()
+      res.status(200).json({
+        message: 'success',
+        itinerary: itinerary.choices[0].text
+      })
+    }catch (err) {
+      console.log('error: ', err)
+    }
+  } catch (err) {
+    console.log('error: ', err)
+  }
+}
